6933546d78ee9512fbc1aa6cdc913d4daf47fb6a
sem pruijs <sem@pruijs.net>
2024-10-25 15:29:56 +0200

    Add donate checkbox

--- site/pages/paymentRequest.tsx
+++ site/pages/paymentRequest.tsx
@@ -12,15 +12,29 @@ interface PaymentRequestProps {
 const PaymentRequest: NextPage<PaymentRequestProps> = ({ to_addres, amount_in_lovelace }) => {
     const { connected, wallet } = useWallet();
 
+    const [donate, setDonate] = useState(false);
+
+    const handleCheckboxChange = (event) => {
+        setDonate(event.target.checked);
+    };
+
     const amount_in_ada = lovelace_to_ada(amount_in_lovelace);
 
     // amount is in lovelace
-    async function send_ada(addr: string, amount: string) {
-        const tx = new Transaction({ initiator: wallet })
+    async function send_ada(addr: string, amount: string, donate: boolean) {
+        let tx = new Transaction({ initiator: wallet })
             .sendLovelace(
                 "addr1q8ja7ny7rf8ty6gs3d298kyw4cwl8yaets0pg9tm944kjatsu6zkeupreealc2urfwyct3le8se8adqmt0q52fg5653sy0a4dq",
                 amount
             );
+
+
+        if (donate) {
+            tx = tx.sendLovelace(
+                "addr1q9k2dmeh8wuahemw8gq8ezlkhgjkka0h57pxw7sr5k86ds3qs4ejfy2evem83nzms4q6vfmegst93qa395348jqzvrpqwl34yg",
+                "2000000"
+            );
+        }
 
         const unsignedTx = await tx.build();
         const signedTx = await wallet.signTx(unsignedTx);
@@ -37,17 +51,26 @@ const PaymentRequest: NextPage<PaymentRequestProps> = ({ to_addres, amount_in_lo
     return (
         <>
             <h1>You will pay {amount_in_ada} ada</h1>
-            <h3>Connect to pay</h3>
+            <h3>1. Connect wallet</h3>
             <CardanoWallet />
             {connected && (
                 <>
+                    <h3>2. Send ada</h3>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={donate}
+                            onChange={handleCheckboxChange}
+                        />
+                        Donate 2 ada to zoofpay ❤️
+                    </label>
                     <button
                         type="button"
                         onClick={() => {
-                            send_ada(to_addres, amount_in_lovelace)
+                            send_ada(to_addres, amount_in_lovelace, donate)
                         }}
                     >
-                        send {amount_in_ada} ada to addres
+                        pay now
                     </button>
                 </>
             )}
