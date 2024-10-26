bf299f088ee3148565356475ff6018f6f735d02c
sem pruijs <sem@pruijs.net>
2024-10-26 19:58:56 +0200

    Make thank you title big

--- site/pages/thankYou.tsx
+++ site/pages/thankYou.tsx
@@ -14,7 +14,7 @@ const ThankYou: NextPage<ThankYouProps> = ({ to_addres, amount_in_lovelace }) =>
     return (
         <>
             <div className="center">
-                <h1>Thank You!</h1>
+                <h1 className="big-title">Thank You!</h1>
                 <p>
                     View transaction on:
                     <a href={cardanoscanLink}>cardanoscan</a>
