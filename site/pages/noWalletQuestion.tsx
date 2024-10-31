f1521284d7251f6b862bd2f398f2e0853619399c
sem pruijs <sem@pruijs.net>
2024-10-31 19:28:02 +0100

    Replace pizzagezond.nl with walletHelp page

--- site/pages/noWalletQuestion.tsx
+++ site/pages/noWalletQuestion.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from "next";
+import Link from 'next/link';
 
 const NoWalletQuestion: NextPage = () => {
     return (
         <p>
             Don&apos;t have a wallet yet?
-            {/* todo make wallet support page. */}
-            <a href="https://pizzagezond.nl">We can help.</a>
+            <Link href="/walletHelp">We can help</Link>
         </p>
     );
 };
---
+++ site/pages/walletHelp.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const WalletHelp: NextPage = () => {
+    return (
+        <>
+            <h1>Wallet help</h1>
+        </>
+    )
+};
+
+export default WalletHelp;
