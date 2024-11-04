fb4afad59c4123963346332d8be09e9cda413cc6
sem pruijs <sem@pruijs.net>
2024-11-04 15:54:18 +0100

    Remove unused dependencies from connnect wallet view

--- site/pages/connectWallet.tsx
+++ site/pages/connectWallet.tsx
@@ -3,12 +3,6 @@ import { useWallet } from '@meshsdk/react';
 import { CardanoWallet } from '@meshsdk/react';
 import { NextPage } from "next";
 import { BrowserWallet, Transaction } from '@meshsdk/core';
-import ThankYou from './thankYou';
-import QRCodeView from "./qrcode";
-import TransactionSummery from "./transaction-summery";
-import NoWalletQuestion from "./noWalletQuestion";
-import DonateToZoofpay from "./donateToZoofpay";
-import NavigatorButtons from "./navigatorButtons";
 
 interface ConnectWalletProps {
     connected: boolean;
