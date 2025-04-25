29e8a52b86778843d828e782c33c26c6eec709bc
Your Name <you@example.com>
2025-04-25 14:34:15 +0200

    show changeAddress

--- site/src/routes/donate/+page.svelte
+++ site/src/routes/donate/+page.svelte
@@ -1,10 +1,21 @@
 <script>
-  	import { CardanoWallet, BrowserWalletState } from '@meshsdk/svelte';
+	import { CardanoWallet, BrowserWalletState } from '@meshsdk/svelte';
+
+  let address = '';
+  let loading = true;
+
+  async function afterConnectedWallet() {
+    if (BrowserWalletState.wallet) {
+      address = await BrowserWalletState.wallet.getChangeAddress();
+    }
+    loading = false;
+  }
 </script>
 <div>
-  <h1 class="bg-purple">Donate</h1>
-  <CardanoWallet />
+  <h1 class="bg-purple">Connect your wallet</h1>
+  <CardanoWallet onConnected={afterConnectedWallet} />
   <h3>About your wallet</h3>
   <p>connected: {BrowserWalletState.connected}</p>
   <p>Your wallet name is: {BrowserWalletState.name}</p>
+  <p>{address}</p>
 </div>
