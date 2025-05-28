eedab5b036c75d8011566c751a2fb7e926daf9fc
Your Name <you@example.com>
2025-05-28 13:17:20 +0200

    create create link flow for automatic version

--- site/src/lib/createLink/AutomaticAddress.svelte
+++ site/src/lib/createLink/AutomaticAddress.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  import { connectedWallet } from "../../stores/wallet";
   import ConnectWallet from "$lib/components/ConnectWallet.svelte";
+  import { CreateLinkStep } from "./createLink";
 
   export let viewState: CreateLinkStep;
 </script>
 <h1>connect wallet</h1>
 <ConnectWallet />
+{#if $connectedWallet}
+<button onclick={() => viewState = CreateLinkStep.EnterAmount}>Next</button>
+{/if}
+<button onclick={() => viewState = CreateLinkStep.ChooseMethod}>Previous</button>
+
